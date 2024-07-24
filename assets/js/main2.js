c9f469c9c36f6407f533251379616da117020cef
Dipayan Das <d.dipayan2002@gmail.com>
2024-07-24 10:22:03 +0530

    fixed header

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -24,11 +24,15 @@ Author: GrayGrids
         var sticky = header_navbar.offsetTop;
         var logo = document.querySelector('.navbar-brand img')
         if (window.pageYOffset > sticky) {
+            header_navbar.classList.add("bg-change");
           header_navbar.classList.add("sticky");
 
+          logo.src = 'ass/lwh.png';
         } else {
           header_navbar.classList.remove("sticky");
+          header_navbar.classList.remove("bg-change");
 
+          logo.src = 'ass/lbh.png';
         }
 
         // show or hide the back-top-top button
@@ -44,32 +48,7 @@ Author: GrayGrids
     new WOW().init();
 
     //===== mobile-menu-btn
-    let navbarToggler = document.querySelector(".mobile-menu-btn");
-    navbarToggler.addEventListener('click', function () {
-        navbarToggler.classList.toggle("active");
-    });
-
-    //======= portfolio-btn active
-	var elements = document.getElementsByClassName("portfolio-btn");
-	for (var i = 0; i < elements.length; i++) {
-		elements[i].onclick = function () {
-
-			// remove class from sibling
-
-			var el = elements[0];
-			while (el) {
-				if (el.tagName === "BUTTON") {
-					//remove class
-					el.classList.remove("active");
-
-				}
-				// pass to the new sibling
-				el = el.nextSibling;
-			}
-
-			this.classList.add("active");
-		};
-	}
+  
 
 
 })();
