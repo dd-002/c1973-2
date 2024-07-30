ab416e19da2deee6dae9dc6e171ad422a1add400
Dipayan Das <d.dipayan2002@gmail.com>
2024-07-31 03:18:27 +0530

    responsiveness issues

---
+++ assets/js/responsive.js
@@ -0,0 +1,5 @@
+const header = document.getElementsByTagName("HEADER")[0];
+var body = document.getElementsByTagName("BODY")[0];
+
+body.style.paddingTop=header.offsetHeight+"px"
+console.log(header.offsetHeight)
